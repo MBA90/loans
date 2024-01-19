20cd68fe07c7d5e191104eb2646e8ae21f66f4f5
Mohammad Abdelhadi <m1990.ah@gmail.com>
2024-01-19 10:37:41 +0400

    commit

---
+++ src/main/java/com/eazybytes/loans/dto/ContactDetailsDTO.java
@@ -0,0 +1,15 @@
+package com.eazybytes.loans.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+@ConfigurationProperties(prefix = "accounts")
+@Setter
+@Getter
+public class ContactDetailsDTO {
+
+    private String name;
+
+    private String email;
+}
